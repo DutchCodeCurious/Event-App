73823be1d856b741ab2023b5bbafee802022a5ce
DutchCodeCurious <114599908+DutchCodeCurious@users.noreply.github.com>
2023-11-06 12:10:22 +0100

    Delete bad event file

--- src/components/PrevEvent.jsx
+++
@@ -1,9 +0,0 @@
-export const PrevEvent = ({ title, description, image }) => {
-  return (
-    <div className="prevEvent">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </div>
-  );
-};
